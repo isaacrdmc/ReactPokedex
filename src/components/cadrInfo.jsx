9d740a6fffd12a463ce70443a0f4bf3347cea0ff
Isaac Ramirez <isaacrdmc@gmail.com>
2025-10-01 08:14:08 -0600

    Card Seccion completada

--- src/components/cadrInfo.jsx
+++ src/components/cadrInfo.jsx
@@ -1,24 +1,49 @@
 
+import { Card, Form, Button } from 'react-bootstrap'; // Si usas react-bootstrap
 
 
 
 export default function CardInfo() {
     return (
-        <div className="card" style={{ width: '18rem' }}> 
-        <img src="https://plus.unsplash.com/premium_photo-1690407617542-2f210cf20d7e?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29uYXxlbnwwfHwwfHx8MA%3D%3D" className="card-img-top" alt="..."></img>
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                </div>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item">An item</li>
-                    <li className="list-group-item">A second item</li>
-                    <li className="list-group-item">A third item</li>
-                </ul>
-                <div className="card-body">
-                    <a href="#" className="card-link">Card link</a>
-                    <a href="#" className="card-link">Another link</a>
-                </div>
-        </div>
+
+        // ? Renderizamos lo siguiente:
+        <Card className='p-3'>
+
+            {/* // * Sección para la Foto */}
+            <div className='d-flex justify-content-center align-items-center mb-3 card-Estilo-sutil'>
+                Foto
+            </div>
+
+            {/* // * Sección del formulario de los Inputis */}
+            <Form>
+                <Form.Group className="mb-2">
+                    <Form.Label><small>Nombre</small></Form.Label>
+                    <Form.Control type="text" size="sm" />
+                </Form.Group>
+                <Form.Group className="mb-2">
+                    <Form.Label><small>Apellido</small></Form.Label>
+                    <Form.Control type="text" size="sm" />
+                </Form.Group>
+                <Form.Group className="mb-2">
+                    <Form.Label><small>Apellido 2</small></Form.Label>
+                    <Form.Control type="text" size="sm" />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label><small>Edad</small></Form.Label>
+                    <Form.Control type="number" size="sm" />
+                </Form.Group>
+            </Form>
+
+
+            {/* // * Sección para los botones de las opciones: */}
+            <div className="d-flex justify-content-between mt-3">
+                <Button className='btn btn-danger'> 
+                    Eliminar 
+                </Button>
+                <Button className='btn btn-success'> 
+                    Agregar 
+                </Button>
+            </div>
+        </Card>
     )
 }
